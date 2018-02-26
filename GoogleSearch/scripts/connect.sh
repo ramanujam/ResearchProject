5596c07bc22008bfdb15d3c974bed4e8376dc262
= <=>
2018-02-26 13:20:08 -0500

    add autodiscover option

--- GoogleSearch/scripts/connect.sh
+++ GoogleSearch/scripts/connect.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo openvpn --config `ls ~/ResearchProject/GoogleSearch/vpn/us*443.ovpn | shuf | head -1` --auth-user-pass auth.txt
+sudo openvpn --redirect-gateway autolocal --config `ls ~/ResearchProject/GoogleSearch/vpn/us*443.ovpn | shuf | head -1` --auth-user-pass auth.txt
