d7ec518c846d75233b5b7dd1aa42cb709626a901
Rohan Singh <rohans1@andrew.cmu.edu>
2018-03-21 17:07:56 -0400

    add chrome options

--- GoogleSearch/src/web2screenshot.py
+++ GoogleSearch/src/web2screenshot.py
@@ -2,16 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
 
-#CHROME_PATH         = '/#Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
+#CHROME_PATH         = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
+#CHROMEDRIVER_PATH   = '/usr/local/bin/chromedriver'
 CHROME_PATH         = '/usr/bin/google-chrome'
 CHROMEDRIVER_PATH   = '/usr/bin/chromedriver'
 WINDOW_SIZE         = "1920x1080"
 
 chrome_options = Options()  
 chrome_options.add_argument("--headless")
-#chrome_options.add_argument("--screenshot")
-# chrome_options.add_argument("--remote-debugging-port=9222")
-
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 chrome_options.binary_location = CHROME_PATH
 
